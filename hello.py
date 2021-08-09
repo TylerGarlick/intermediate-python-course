ce2993fc4fd7c62ec14c1adaa19304121a17251a
Tyler Garlick <tjgarlick@gmail.com>
2021-08-09 09:50:17 -0600

    Added template code

--- hello.py
+++ hello.py
@@ -4,6 +4,10 @@ import argparse
 
 
 def get_args():
+    """
+    Gets the arguments from the command line
+    :return:  list of parse arguments
+    """
     parser = argparse.ArgumentParser(description='Say hello')
     parser.add_argument('-n', '--name', help='Your name', metavar='name', default='World')
     return parser.parse_args()
--- readme.md
+++ readme.md
@@ -2,4 +2,5 @@
 
 * Test File for `hello.py` (https://icarus.cs.weber.edu/~hvalle/hafb/test_hello.py)
     * `pip install pytest`
+* Template File (https://icarus.cs.weber.edu/~hvalle/hafb/template.py)
 
