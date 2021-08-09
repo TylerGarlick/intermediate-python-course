1ea3397fe00e350e73ad735092dad48842a13060
Tyler Garlick <tjgarlick@gmail.com>
2021-08-09 09:17:19 -0600

    CLI program

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Say hello')
-parser.add_argument('name', help='Your name')
+parser.add_argument('-n', '--name', help='Your name', metavar='name', default='World')
 args = parser.parse_args()
 print('Hello, ' + args.name)
