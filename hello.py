666bafbb7317b3e8483c7e73442662537e95680b
Tyler Garlick <tjgarlick@gmail.com>
2021-08-09 10:35:33 -0600

    Added article logic

--- hello.py
+++ hello.py
@@ -18,7 +18,7 @@ def get_args():
 def main():
     args = get_args()
     word = args.word
-    article = ''
+    article = 'an' if word[0].lower() in 'aeiou' else 'a'
     print(f'Ahoy, Captain, {article} {word} off the larboard bow!')
 
 
